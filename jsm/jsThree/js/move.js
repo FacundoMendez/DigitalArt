5c2d4eeb30d42e14eca833d6ef5a644ef6c54058
FacundoMendez <facundo.mendez@live.com.ar>
2021-11-10 11:14:19 -0300

    efectos de transicion con GSAP

---
+++ jsm/jsThree/js/move.js
@@ -0,0 +1,161 @@
+gsap.registerPlugin(ScrollTrigger);
+
+
+gsap.from(".container2",{
+    duration:2,
+    pin:true,
+    backgroundColor: '#ffffff',
+    x:"-=2000",
+    ease:"linear",
+    scrollTrigger:{
+        trigger: ".container2",
+        markers:true,
+        start: "top 50%",
+        end: "100% 100%",
+        pinSpacing:true,
+        scrub: 1,
+    }
+})
+
+gsap.from(".container3__card-webgl",{
+    duration:2,
+    opacity:0,
+    x:"-=1000",
+    pin:true,
+    ease:"linear",
+    scrollTrigger:{
+        trigger: ".container3__card-webgl",
+        markers:true,
+        start: "top 50%",
+        end: "100% 100%",
+        pinSpacing:true,
+        scrub: 1,
+    }
+})
+
+gsap.from(".container3__card-webgl2",{
+    duration:2,
+    opacity:0,
+    x:"+=1000",
+    pin:true,
+    ease:"linear",
+    scrollTrigger:{
+        trigger: ".container3__card-webgl2",
+        markers:true,
+        start: "top 50%",
+        end: "100% 100%",
+        pinSpacing:true,
+        scrub: 1,
+    }
+})
+
+
+gsap.from(".container3__card-webgl4",{
+    duration:3,
+    opacity:0,
+    x:"-=1000",
+    pin:true,
+    ease:"linear",
+    scrollTrigger:{
+        trigger: ".container3__card-webgl4",
+        markers:true,
+        start: "top 50%",
+        end: "100% 100%",
+        pinSpacing:true,
+        scrub: 1,
+    }
+})
+
+
+gsap.from(".container3__card-webgl5",{
+    duration:3,
+    opacity:0,
+    x:"+=1000",
+    pin:true,
+    ease:"linear",
+    scrollTrigger:{
+        trigger: ".container3__card-webgl5",
+        markers:true,
+        start: "top 50%",
+        end: "100% 100%",
+        pinSpacing:true,
+        scrub: 1,
+    }
+})
+
+
+gsap.from(".container3__card-webgl6",{
+    duration:3,
+    opacity:0,
+    x:"-=1000",
+    pin:true,
+    ease:"linear",
+    scrollTrigger:{
+        trigger: ".container3__card-webgl6",
+        markers:true,
+        start: "top 50%",
+        end: "100% 100%",
+        pinSpacing:true,
+        scrub: 1,
+    }
+})
+
+
+
+gsap.from(".container3__card-webgl8",{
+    duration:3,
+    opacity:0,
+    x:"+=1000",
+    pin:true,
+    ease:"linear",
+    scrollTrigger:{
+        trigger: ".container3__card-webgl8",
+        markers:true,
+        start: "top 50%",
+        end: "100% 100%",
+        pinSpacing:true,
+        scrub: 1,
+    }
+})
+
+
+gsap.from(".container3__card-webgl9",{
+    duration:3,
+    opacity:0,
+    x:"-=1000",
+    pin:true,
+    ease:"linear",
+    scrollTrigger:{
+        trigger: ".container3__card-webgl9",
+        markers:true,
+        start: "top 50%",
+        end: "100% 100%",
+        pinSpacing:true,
+        scrub: 1,
+    }
+})
+
+
+gsap.from(".container3__card-webgl10",{
+    duration:3,
+    opacity:0,
+    x:"+=1000",
+    pin:true,
+    ease:"linear",
+    scrollTrigger:{
+        trigger: ".container3__card-webgl10",
+        markers:true,
+        start: "top 50%",
+        end: "100% 100%",
+        pinSpacing:true,
+        scrub: 1,
+    }
+})
+
+
+gsap.from("main",{
+    opacity:-5,
+    duration:2,
+    y: -300,
+})
+
