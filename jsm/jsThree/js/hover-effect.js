87bdab7127b0ab0e563b9ac37b1bfed0cf613f9c
Facundo Mendez <facundo.mendez@live.com.ar>
2021-11-22 22:58:05 -0300

    progreso banner nft

---
+++ jsm/jsThree/js/hover-effect.js
@@ -0,0 +1,7 @@
+var myAnimation = new hoverEffect({
+    parent: document.querySelector('.banner__img'),
+    intensity: 0.3,
+    image1: '../img/banner5.png',
+    image2: '../img/banner5.png',
+    displacementImage: '../img/1.jpg',
+});
