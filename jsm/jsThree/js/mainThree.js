7a85530908daef5351159afdcca6c430fe6a126b
Facundo Mendez <facundo.mendez@live.com.ar>
2021-11-27 02:51:51 -0300

    mas optimizacion de scripts

--- jsm/jsThree/js/mainThree.js
+++ jsm/jsThree/js/mainThree.js
@@ -1,3 +1,215 @@
+import { OrbitControls } from '../three.js-master/examples/jsm/controls/OrbitControls.js';
+
+
+(() => {
+    const canvas = document.querySelector('.planetWebGL')
+
+    // scene setup
+    const scene = new THREE.Scene();
+    
+    const size = {
+        width :  window.innerWidth,
+        height : window.innerHeight
+    }
+    
+    window.addEventListener ('resize', () => {
+        size.width = window.innerWidth
+        size.height = window.innerHeight
+    
+        camera.aspect = size.width / size.height
+        camera.updateProjectionMatrix()
+    
+        renderer.setSize ( size.width , size.height)
+        renderer.setPixelRatio(Math.min(window.devicePixelRatio),2)
+    })
+    
+    const camera = new THREE.PerspectiveCamera(60, size.width/size.height, 0.1,1000)
+    camera.position.z = 3
+    scene.add(camera)
+    
+    // renderer setup
+    const renderer = new THREE.WebGLRenderer({
+        canvas: canvas,
+        antialias: true,
+    });
+    renderer.setSize ( size.width , size.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio),2)
+    
+    // orbit control setup
+     const controls = new OrbitControls(camera,canvas);
+    controls.enableDamping = true
+    controls.enableZoom = false 
+    controls.enablePan= false
+    
+    // ambient light
+    const ambientlight = new THREE.AmbientLight(0xffffff, 0.2);
+    scene.add(ambientlight);
+    
+    // point light
+    const pointLight = new THREE.PointLight(0xffffff, 1)
+    pointLight.position.set(5, 3, 5);
+    scene.add(pointLight);
+    
+    
+    // earth geometry
+    const earthGeometry = new THREE.SphereBufferGeometry(0.6, 32, 32);
+    
+    // earth material
+    const earthMaterial = new THREE.MeshPhongMaterial({
+        map: THREE.ImageUtils.loadTexture('./jsm/jsThree/img/texturePlaneta/earthmap1k.jpg'),
+        bumpMap: THREE.ImageUtils.loadTexture('./jsm/jsThree/img/texturePlaneta/earthbump.jpg'),
+        bumpScale: 0.2,
+        transparent: true,
+    });
+    
+    // earth mesh
+    const earthMesh = new THREE.Mesh(earthGeometry, earthMaterial);
+    
+    
+    scene.add(earthMesh);
+    
+    // cloud Geometry
+    const cloudGeometry = new THREE.SphereGeometry(0.63, 32, 32);
+    
+    // cloud metarial
+    const cloudMetarial = new THREE.MeshPhongMaterial({
+        map: THREE.ImageUtils.loadTexture('./jsm/jsThree/img/texturePlaneta/earthCloud.png'),
+        transparent: true,
+    });
+    
+    // cloud mesh
+    const cloudMesh = new THREE.Mesh(cloudGeometry, cloudMetarial);
+    
+    scene.add(cloudMesh);
+    
+    const textureLoader = new THREE.TextureLoader()
+    
+    const starTexture = textureLoader.load('./jsm/jsThree/img/star3.webp')
+    
+    const geometry = new THREE.BufferGeometry()
+    const count = 700
+    const position = new Float32Array(count * 3)
+    for (let i = 0; i < count *3; i++) {
+     
+        position[i] =(Math.random()- 0.5) * 9
+    }
+    
+    geometry.setAttribute("position", new THREE.BufferAttribute(position,3))
+    
+    const material = new THREE.PointsMaterial({
+        size:0.3,
+        sizeAttenuation: true,
+        color: new THREE.Color("#4DaaFF"),
+        map:starTexture,
+        alphaMap:starTexture,
+        transparent: true,
+        depthTest:false,
+    })
+    
+    const particle = new THREE.Points(geometry,material)
+    particle.position.z= -1
+    scene.add(particle)
+    
+    const clock = new THREE.Clock()
+    
+    const animate = () =>{
+        earthMesh.rotation.y += -0.00009 
+        particle.rotation.y += -0.0009
+        particle.rotation.z += 0.0009
+    
+        earthMesh.rotation.y -= 0.0015;
+        cloudMesh.rotation.y -= 0.0019;
+        controls.update()
+        renderer.render(scene,camera)
+        window.requestAnimationFrame(animate)
+    
+    }
+    
+    animate()
+    renderer.render(scene,camera)
+})();
+
+
+
+(() => {
+    const canvas = document.querySelector('.starsWebGL')
+
+    // scene setup
+    const scene = new THREE.Scene();
+    
+    
+    const size = {
+        width :  window.innerWidth,
+        height : window.innerHeight
+    }
+    
+    window.addEventListener ('resize', () => {
+        size.width = window.innerWidth
+        size.height = window.innerHeight
+    
+        camera.aspect = size.width / size.height
+        camera.updateProjectionMatrix()
+    
+        renderer.setSize ( size.width , size.height)
+        renderer.setPixelRatio(Math.min(window.devicePixelRatio),2)
+    })
+    
+    const camera = new THREE.PerspectiveCamera(60, size.width/size.height, 0.1,1000)
+    camera.position.z = 2
+    camera.rotation.x = Math.PI/2;
+    scene.add(camera)
+    
+    // renderer setup
+    const renderer = new THREE.WebGLRenderer({
+        canvas: canvas,
+        antialias: true,
+    });
+    
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio),2)
+    renderer.setSize ( size.width , size.height, false)
+    
+    const textureLoader = new THREE.TextureLoader()
+    
+    const starTexture = textureLoader.load('./jsm/jsThree/img/star3.webp')
+    
+    const geometry = new THREE.BufferGeometry()
+    const count = 1000
+    const position = new Float32Array(count * 3)
+    for (let i = 0; i < count * 3; i++) {
+        position[i] =(Math.random()- 0.5) * 9
+    }
+    
+    geometry.setAttribute("position", new THREE.BufferAttribute(position,3))
+    
+    const material = new THREE.PointsMaterial({
+        size:0.3,
+        sizeAttenuation: true,
+        color: new THREE.Color("#ff00ff"),
+        map:starTexture,
+        alphaMap:starTexture,
+        transparent: true,
+        depthTest:false
+    })
+    
+    const particle = new THREE.Points(geometry,material)
+    scene.add(particle)
+    
+    const animate = () =>{
+        particle.rotation.x += -0.0001 
+        particle.rotation.y += 0.001
+        particle.rotation.z += 0.0003 
+    
+        renderer.render(scene,camera)
+        window.requestAnimationFrame(animate)
+        renderer.autoClear = true
+    }
+    
+    animate()
+    
+    renderer.render(scene,camera)
+    
+})();
+
 
 (() => {
     const canvas = document.querySelector('.container3__card-webgl')
--- jsm/jsThree/js/move.js
+++ jsm/jsThree/js/move.js
@@ -1,286 +1,290 @@
-gsap.registerPlugin (ScrollTrigger);
-gsap.registerPlugin( CSSPlugin )
-
-
-const card1 = document.querySelector(".card1")
-const card2 = document.querySelector(".card2")
-const card3 = document.querySelector(".card3")
-const card4 = document.querySelector(".card4")
-const card5 = document.querySelector(".card5")
-const card6 = document.querySelector(".card6")
-
 /* MOVILE CARD */
+(() =>{
+    let preload = document.querySelector(".preload");
 
-ScrollTrigger.matchMedia({
-    "(min-width: 800px)":function(){
+    setTimeout(function(){
+        preload.classList.add("cerrar");
+        preload.style.zIndex=0;
+    },3000)
+    
+})();
 
-        gsap.from("main",{
-            opacity:-2,
-            duration:4.3,
-            y: -200,
-        })
-        
-        /* Header */
-        gsap.from(".nav__logo",{
-            opacity:0,
-            duration:4.3,
-            x: -400,
-        })
-        
-        gsap.from(".nav__listEnlaces",{
-            opacity:0,
-            duration:4.3,
-            y: -200,
-        })
-        
+(() =>{
+    gsap.registerPlugin (ScrollTrigger);
+    gsap.registerPlugin( CSSPlugin )
+    const card1 = document.querySelector(".card1")
+    const card2 = document.querySelector(".card2")
+    const card3 = document.querySelector(".card3")
+    const card4 = document.querySelector(".card4")
+    const card5 = document.querySelector(".card5")
+    const card6 = document.querySelector(".card6")
+    ScrollTrigger.matchMedia({
 
-        gsap.from(card1,{
-            duration:3,
-            opacity:0,
-            x:"-=1000",
-            ease:"linear",
-            scrollTrigger:{
-                trigger: card1,
-                start: "top 100%",
-                end: "100% 100%",
-                pinSpacing:true,
-                scrub: 2,
-            }
-        })
-
-        gsap.from(card1,{
-            duration:3,
-            opacity:0,
-            x:"-=1000",
-            ease:"linear",
-            scrollTrigger:{
-                trigger: card1,
-                start: "top 100%",
-                end: "100% 100%",
-                pinSpacing:true,
-                scrub: 2,
-            }
-        })
-        
-        gsap.from(card2,{
-            duration:3,
-            opacity:0,
-            x:"+=1000",
-            ease:"linear",
-            scrollTrigger:{
-                trigger: card2,
-                start: "top 100%",
-                end: "100% 100%",
-                pinSpacing:true,
-                scrub: 2,
-            }
-        })
-        
-        
-        gsap.from(card3,{
-            duration:3,
-            opacity:0,
-            x:"-=1000",
-            ease:"linear",
-            scrollTrigger:{
-                trigger: card3,
-                start: "top 100%",
-                end: "100% 100%",
-                pinSpacing:true,
-                scrub: 2,
-            }
-        })
-        
-        
-        gsap.from(card4,{
-            duration:3,
-            opacity:0,
-            x:"+=1000",
-            ease:"linear",
-            scrollTrigger:{
-                trigger: card4,
-                start: "top 100%",
-                end: "100% 100%",
-                pinSpacing:true,
-                scrub: 2,
-            }
-        })
-        
-        
-        gsap.from(card5,{
-            duration:3,
-            opacity:0,
-            x:"-=1000",
-            ease:"linear",
-            scrollTrigger:{
-                trigger: card5,
-                start: "top 100%",
-                end: "100% 100%",
-                pinSpacing:true,
-                scrub: 2,
-            }
-        })
-        
-        
-        gsap.from(card6,{
-            duration:3,
-            opacity:0,
-            x:"+=1000",
-            ease:"linear",
-            scrollTrigger:{
-                trigger: card6,
-                start: "top 100%",
-                end: "100% 100%",
-                pinSpacing:true,
-                scrub: 2,
-            }
-        })
-        
-        gsap.from(".container3__title",{
-            opacity:-4,
-            scale:.5,
-            scrollTrigger:{
-                trigger: ".container3__title",
-                start: "top 80%",
-                end: "100% 100%",
-                scrub: 4,
-            }
-        })
-        
-        gsap.from(".container3__text",{
-            opacity:-4,
-            scale:.5,
-            scrollTrigger:{
-                trigger: ".container3__text",
-                start: "top 80%",
-                end: "100% 100%",
-                scrub: 4,
-            }
-        })
-        
-        gsap.from(".starsWebGL",{
-            opacity:-2,
-            scale:.5,
-            scrollTrigger:{
-                trigger: ".starsWebGL",
-                start: "top 100%",
-                end: "100% 100%",
-                scrub: 4,
-            }
-        })
-        
-        
-        /* cards */
-        
-        gsap.from(".container3__card-webgl",{
-            opacity:-5,
-            x: 600,
-            ease: "Back.easeOut",
-            scrollTrigger:{
-                trigger: ".container3__card-webgl",
-                start: "top 100%",
-                end: "100% 100%",
-                scrub: 4,
-            }
-        })
-        
-        gsap.from(".container3__card-webgl2",{
-            opacity:-5,
-            x: 600,
-            ease: "Back.easeOut",
-            scrollTrigger:{
-                trigger: ".container3__card-webgl2",
-                start: "top 100%",
-                end: "100% 100%",
-                scrub: 4,
-            }
-        })
-        
-        gsap.from(".container3__card-webgl4",{
-            opacity:-5,
-            x: 600,
-            ease: "Back.easeOut",
-            scrollTrigger:{
-                trigger: ".container3__card-webgl4",
-                start: "top 100%",
-                end: "100% 100%",
-                scrub: 4,
-            }
-        })
-        
-        gsap.from(".container3__card-webgl6",{
-            opacity:-5,
-            x: 600,
-            ease:"Back.easeOut",
-            scrollTrigger:{
-                trigger: ".container3__card-webgl6",
-                start: "top 100%",
-                end: "100% 100%",
-                scrub: 4,
-            }
-        })
-        
-        /* CONTAINER 4 */
-        
-        gsap.from(".container4",{
-            opacity:-3,
-            scale:2,
-            scrollTrigger:{
-                trigger: ".container4",
-                start: "top 100%",
-                end: "100% 100%",
-                scrub: 2,
-            }
-        })
-        
-        gsap.from(".container4__title",{
-            opacity:-3,
-            scale:2,
-            scrollTrigger:{
-                trigger: ".container4__title",
-                start: "top 100%",
-                end: "100% 100%",
-                scrub:1.8,
-            }
-        })
-        
-        gsap.from(".container4__text2",{
-            opacity:-3,
-            x: 600,
-            scrollTrigger:{
-                trigger: ".container4__text2",
-                start: "top 60%",
-                end: "100% 100%",
-                scrub: 2.2,
-            }
-        })
-        
-        gsap.from(".container4__text1",{
-            opacity:-2,
-            x: -600,
-            scrollTrigger:{
-                trigger: ".container4__text1",
-                start: "top 60%",
-                end: "100% 100%",
-                scrub: 2.2,
-            }
-        })
-        
-        gsap.from(".container4__line",{
-            opacity:-2,
-            y: 300,
-            ease: "Back.easeOut",
-            scrollTrigger:{
-                trigger: ".container4__line",
-                start: "top 150%",
-                end: "100% 100%",
-                scrub: 2.2,
-            }
-        })
-        
-        
-        
-    }
-})
-
-
+        "(min-width: 800px)":function(){
+    
+            gsap.from("main",{
+                opacity:-2,
+                duration:4.3,
+                y: -200,
+            })
+            
+            /* Header */
+            gsap.from(".nav__logo",{
+                opacity:0,
+                duration:4.3,
+                x: -400,
+            })
+            
+            gsap.from(".nav__listEnlaces",{
+                opacity:0,
+                duration:4.3,
+                y: -200,
+            })
+            
+    
+            gsap.from(card1,{
+                duration:3,
+                opacity:0,
+                x:"-=1000",
+                ease:"linear",
+                scrollTrigger:{
+                    trigger: card1,
+                    start: "top 100%",
+                    end: "100% 100%",
+                    pinSpacing:true,
+                    scrub: 2,
+                }
+            })
+    
+            gsap.from(card1,{
+                duration:3,
+                opacity:0,
+                x:"-=1000",
+                ease:"linear",
+                scrollTrigger:{
+                    trigger: card1,
+                    start: "top 100%",
+                    end: "100% 100%",
+                    pinSpacing:true,
+                    scrub: 2,
+                }
+            })
+            
+            gsap.from(card2,{
+                duration:3,
+                opacity:0,
+                x:"+=1000",
+                ease:"linear",
+                scrollTrigger:{
+                    trigger: card2,
+                    start: "top 100%",
+                    end: "100% 100%",
+                    pinSpacing:true,
+                    scrub: 2,
+                }
+            })
+            
+            
+            gsap.from(card3,{
+                duration:3,
+                opacity:0,
+                x:"-=1000",
+                ease:"linear",
+                scrollTrigger:{
+                    trigger: card3,
+                    start: "top 100%",
+                    end: "100% 100%",
+                    pinSpacing:true,
+                    scrub: 2,
+                }
+            })
+            
+            
+            gsap.from(card4,{
+                duration:3,
+                opacity:0,
+                x:"+=1000",
+                ease:"linear",
+                scrollTrigger:{
+                    trigger: card4,
+                    start: "top 100%",
+                    end: "100% 100%",
+                    pinSpacing:true,
+                    scrub: 2,
+                }
+            })
+            
+            
+            gsap.from(card5,{
+                duration:3,
+                opacity:0,
+                x:"-=1000",
+                ease:"linear",
+                scrollTrigger:{
+                    trigger: card5,
+                    start: "top 100%",
+                    end: "100% 100%",
+                    pinSpacing:true,
+                    scrub: 2,
+                }
+            })
+            
+            
+            gsap.from(card6,{
+                duration:3,
+                opacity:0,
+                x:"+=1000",
+                ease:"linear",
+                scrollTrigger:{
+                    trigger: card6,
+                    start: "top 100%",
+                    end: "100% 100%",
+                    pinSpacing:true,
+                    scrub: 2,
+                }
+            })
+            
+            gsap.from(".container3__title",{
+                opacity:-4,
+                scale:.5,
+                scrollTrigger:{
+                    trigger: ".container3__title",
+                    start: "top 80%",
+                    end: "100% 100%",
+                    scrub: 4,
+                }
+            })
+            
+            gsap.from(".container3__text",{
+                opacity:-4,
+                scale:.5,
+                scrollTrigger:{
+                    trigger: ".container3__text",
+                    start: "top 80%",
+                    end: "100% 100%",
+                    scrub: 4,
+                }
+            })
+            
+            gsap.from(".starsWebGL",{
+                opacity:-2,
+                scale:.5,
+                scrollTrigger:{
+                    trigger: ".starsWebGL",
+                    start: "top 100%",
+                    end: "100% 100%",
+                    scrub: 4,
+                }
+            })
+            
+            
+            /* cards */
+            
+            gsap.from(".container3__card-webgl",{
+                opacity:-5,
+                x: 600,
+                ease: "Back.easeOut",
+                scrollTrigger:{
+                    trigger: ".container3__card-webgl",
+                    start: "top 100%",
+                    end: "100% 100%",
+                    scrub: 4,
+                }
+            })
+            
+            gsap.from(".container3__card-webgl2",{
+                opacity:-5,
+                x: 600,
+                ease: "Back.easeOut",
+                scrollTrigger:{
+                    trigger: ".container3__card-webgl2",
+                    start: "top 100%",
+                    end: "100% 100%",
+                    scrub: 4,
+                }
+            })
+            
+            gsap.from(".container3__card-webgl4",{
+                opacity:-5,
+                x: 600,
+                ease: "Back.easeOut",
+                scrollTrigger:{
+                    trigger: ".container3__card-webgl4",
+                    start: "top 100%",
+                    end: "100% 100%",
+                    scrub: 4,
+                }
+            })
+            
+            gsap.from(".container3__card-webgl6",{
+                opacity:-5,
+                x: 600,
+                ease:"Back.easeOut",
+                scrollTrigger:{
+                    trigger: ".container3__card-webgl6",
+                    start: "top 100%",
+                    end: "100% 100%",
+                    scrub: 4,
+                }
+            })
+            
+            /* CONTAINER 4 */
+            
+            gsap.from(".container4",{
+                opacity:-3,
+                scale:2,
+                scrollTrigger:{
+                    trigger: ".container4",
+                    start: "top 100%",
+                    end: "100% 100%",
+                    scrub: 2,
+                }
+            })
+            
+            gsap.from(".container4__title",{
+                opacity:-3,
+                scale:2,
+                scrollTrigger:{
+                    trigger: ".container4__title",
+                    start: "top 100%",
+                    end: "100% 100%",
+                    scrub:1.8,
+                }
+            })
+            
+            gsap.from(".container4__text2",{
+                opacity:-3,
+                x: 600,
+                scrollTrigger:{
+                    trigger: ".container4__text2",
+                    start: "top 60%",
+                    end: "100% 100%",
+                    scrub: 2.2,
+                }
+            })
+            
+            gsap.from(".container4__text1",{
+                opacity:-2,
+                x: -600,
+                scrollTrigger:{
+                    trigger: ".container4__text1",
+                    start: "top 60%",
+                    end: "100% 100%",
+                    scrub: 2.2,
+                }
+            })
+            
+            gsap.from(".container4__line",{
+                opacity:-2,
+                y: 300,
+                ease: "Back.easeOut",
+                scrollTrigger:{
+                    trigger: ".container4__line",
+                    start: "top 150%",
+                    end: "100% 100%",
+                    scrub: 2.2,
+                }
+            })
+        }
+    })
+})();
