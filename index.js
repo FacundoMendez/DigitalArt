74fbe0b415ff5335a939f2374a5637671ad03c5f
Facundo Mendez <facundo.mendez@live.com.ar>
2021-11-28 03:46:00 -0300

    arreglo responsive COLLECTIONS

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const navToggle = document.querySelector(".nav__nav-toggle");
 const navMenu = document.querySelector(".nav__menu");
 const navVisible = document.querySelector(".navVisible")
-
+const navimg = document.querySelector(".nav__list")
 
 navToggle.addEventListener("click", () => {
   navMenu.classList.toggle("navVisible");
@@ -11,12 +11,12 @@ navToggle.addEventListener("click", () => {
   if (navMenu.classList.contains("navVisible")) {
     navToggle.setAttribute("aria-label", "Cerrar menu");
     navVisible.style.left= "0%";
-
+    navimg.style.background= "black";
   } 
   else {
     navToggle.setAttribute("aria-label", "Abrir menu");
     navVisible.style.left= "100%";
-
+    navimg.style.background= "transparent";
   }
 });
 
